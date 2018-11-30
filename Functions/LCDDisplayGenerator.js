7f7274d1aa13bdf13721867a4e8bfd71ab2842e5
Adel Kovacs <kovacsadel12@gmail.com>
2018-11-30 14:09:42 +0100

    init commit

---
+++ Functions/LCDDisplayGenerator.js
@@ -0,0 +1,26 @@
+module.exports = {
+    generate(number) {
+        if (!Number.isInteger(number)) {
+            throw('Invalid input')
+        }
+
+        return numbers[number];
+
+    }
+};
+
+const numbers_ASCII = `
+._.   ...   ._.   ._.   ...   ._.   ._.   ._.   ._.   ._.
+|.|   ..|   ._|   ._|   |_|   |_.   |_.   ..|   |_|   |_|
+|_|   ..|   |_.   ._|   ..|   ._|   |_|   ..|   |_|   ..|
+
+`;
+
+const numbers = {
+    0: `
+    ._.
+    |.|
+    |_|
+    `
+};
+
---
+++ __tests__/LCDDisplayGenerator.spec.js
@@ -0,0 +1,33 @@
+const LCDGenerator = require('../Functions/LCDDisplayGenerator');
+
+
+describe("LCDDisplayGenerator", () => {
+    test("should throw invalid input error for string arg", () => {
+        expect(() => {
+            LCDGenerator.generate('SadString')
+                .toThrow("Invalid input");
+        })
+    });
+
+    test("0 should return proper string", () => {
+        expect(() => {
+            LCDGenerator.generate(0)
+                .toBe(` 
+                ._.
+                |.|
+                |_|
+       `)}
+        )
+    })
+
+    test("12 should return proper string", () => {
+        expect(()=> {
+            LCDGenerator(12)
+                .toBe(`
+                ... ._.
+                ..| ._|
+                ..| |_.
+                `)
+        })
+    })
+});
